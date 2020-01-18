6c219687098869a6c1e34bd52b2e9a49c809d98e
martin <zakyz.martin@gmail.com>
2020-01-18 23:10:03 +0100

    lists index with fake data done

--- app/src/main/java/com/example/pricetag/ui/shops/ShopsFragment.java
+++ app/src/main/java/com/example/pricetag/ui/shops/ShopsFragment.java
@@ -8,45 +8,22 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pricetag.IndexFragment;
 import com.example.pricetag.R;
-import com.example.pricetag.templates.adapters.IndexAdapter;
-import com.example.pricetag.ui.shops.ShopsViewModel;
 
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class ShopsFragment extends Fragment {
 
-    private ShopsViewModel shopsViewModel;
-
-    @BindView(R.id.headingTextView)
-    TextView headingTextView;
-
-    @BindView(R.id.searchTextView)
-    TextView searchTextView;
-
-    @BindView(R.id.searchButton)
-    Button searchButton;
-
-    @BindView(R.id.recyclerView)
-    RecyclerView recyclerView;
-
-    private RecyclerView.Adapter indexAdapter;
-    private RecyclerView.LayoutManager layoutManager;
     private List<String> data;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
